dc30e14842a2b679528ff949b885e765ee6d37f6
1756096090 <84259949+1756096090@users.noreply.github.com>
2025-01-20 17:39:42 -0500

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { ReportExpensesComponent } from '../components/report-expenses/report-expenses.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, ReportExpensesComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
